c16f978c1039151d3f6ecf803cf7eb3118331a49
GILL SADHU PRIYANK <138834035+Gill817@users.noreply.github.com>
2023-11-30 18:10:36 -0500

    Update APPS.py

--- APPS.py
+++ APPS.py
@@ -9,7 +9,7 @@ page_bg = '''
 body {
   background-image: url("https://images.unsplash.com/photo-1584036561566-baf8f5f1b144?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
   background-size: cover;
-  opacity: 0.6;
+  opacity: 0.7;
   font-color: white;
 }
 </style>
